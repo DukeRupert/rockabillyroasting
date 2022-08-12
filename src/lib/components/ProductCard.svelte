006defa9b35926578b847927dd1059db9e1b9c19
Logan Williams <logan@firefly.llc>
2022-08-12 08:50:30 -0600

    Created ProductCard component

---
+++ src/lib/components/ProductCard.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { Product } from '$lib/types/woocommerce/products';
+
+	export let data: Product;
+	console.log(data);
+</script>
+
+<a href="#" class="group">
+	<div class="w-full aspect-w-1 aspect-h-1 max-h-80 rounded-lg overflow-hidden">
+		<img
+			src={data.images[0].src}
+			alt={data.images[0].alt}
+			class="w-full h-full object-center object-cover group-hover:opacity-75"
+			height="300"
+			width="300"
+		/>
+	</div>
+	<div class="mt-4 flex items-center justify-between text-base font-medium text-gray-900">
+		<h3>{data.name}</h3>
+		<p>${data.price}</p>
+	</div>
+	<p class="mt-1 text-sm italic text-gray-500">3 sizes available</p>
+</a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,9 +5,9 @@
 			headers: {
 				'Content-Type': 'application/json'
 			},
-			body: JSON.stringify({ page: 1, per_page: 15 })
+			body: JSON.stringify({ page: 1, per_page: 3 })
 		});
-		const data = await response.json();
+		const { data } = await response.json();
 		return {
 			status: 200,
 			props: {
@@ -18,7 +18,21 @@
 </script>
 
 <script lang="ts">
-	export let data;
+	import ProductCard from '$lib/components/ProductCard.svelte';
+	import type { Products } from '$lib/types/woocommerce/products';
+	export let data: Products;
+	console.log(data);
 </script>
 
-<pre>{JSON.stringify(data)}</pre>
+<!-- Product grid -->
+<section aria-labelledby="products-heading" class="mt-8">
+	<h2 id="products-heading" class="sr-only">Products</h2>
+
+	<div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:gap-x-8">
+		{#each data as product}
+			<ProductCard data={product} />
+		{/each}
+
+		<!-- More products... -->
+	</div>
+</section>
